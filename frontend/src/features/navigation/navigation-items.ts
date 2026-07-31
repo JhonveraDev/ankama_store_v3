3af776a252733947daf13cb79e6d8aea99ee936f
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-07-31 15:07:28 -0500

    feat(Products): update product details and add new subscription options

--- frontend/src/features/navigation/navigation-items.ts
+++ frontend/src/features/navigation/navigation-items.ts
@@ -5,11 +5,11 @@ export interface NavigationItem {
 }
 
 export const navigationItems: NavigationItem[] = [
-  { label: 'DOFUS', logoUrl: '/media/brand/nav-logos/nav_item_dofus.png', categories: ['Suscripciones', 'Ogrinas', 'Servicios', 'Ofertas actuales', 'Objetos en juego'] },
-  { label: 'DOFUS Retro', logoUrl: '/media/brand/nav-logos/nav_item_dofus_retro.png', categories: ['Suscripciones', 'Ogrinas', 'Servicios', 'Ofertas actuales', 'Objetos en juego'] },
-  { label: 'WAKFU', logoUrl: '/media/brand/nav-logos/nav_item_wakfu.png', categories: ['Suscripciones', 'Ogrinas', 'Servicios', 'Ofertas actuales', 'Objetos en juego'] },
-  { label: 'WAVEN', logoUrl: '/media/brand/nav-logos/nav_item_waven.png', categories: ['Packs', 'Ofertas actuales', 'Objetos en juego'] },
-  { label: 'KROSMAGA', logoUrl: '/media/brand/nav-logos/nav_item_krosmaga.png', categories: ['Packs', 'Ofertas actuales', 'Objetos en juego'] },
-  { label: 'Premium Games', logoUrl: '/media/brand/nav-logos/nav_item_premium_games.png', categories: ['Juegos', 'Ofertas actuales'] },
-  { label: 'Webtoons', logoUrl: '/media/brand/nav-logos/nav_item_webtoons.png', categories: ['Novedades', 'Series', 'Ofertas actuales'] },
+  { label: 'DOFUS', logoUrl: '/media/brand/nav-logos/nav_item_dofus.png', categories: ['Ofertas de la semana', 'Abononos', 'Ofertas del momento', 'Ogrinas', 'Servicios', 'Los clásicos atemporales', 'Colección de verano', 'Aperiencias de clases', 'Mercado de ocaciones', 'Peluches'] },
+  { label: 'DOFUS Retro', logoUrl: '/media/brand/nav-logos/nav_item_dofus_retro.png', categories: ['Abonos', 'Ogrinas', 'Servicios', 'Ofertas del momento', 'Objetos en el juego'] },
+  { label: 'WAKFU', logoUrl: '/media/brand/nav-logos/nav_item_wakfu.png', categories: ['Boosters', 'Ogrinas', 'Servicios', 'Paquetes', 'Objetos', 'Ventanas actuales', 'Piedra de kamas', 'Peluches'] },
+  { label: 'WAVEN', logoUrl: '/media/brand/nav-logos/nav_item_waven.png', categories: ['Packs', 'Gemas'] },
+  { label: 'KROSMAGA', logoUrl: '/media/brand/nav-logos/nav_item_krosmaga.png', categories: ['Kamas', 'Paquetes', 'Pedestales'] },
+  { label: 'Premium Games', logoUrl: '/media/brand/nav-logos/nav_item_premium_games.png', categories: ['Savara', 'One more gate', 'Maliki', 'Super nano blaster'] },
+  { label: 'Webtoons', logoUrl: '/media/brand/nav-logos/nav_item_webtoons.png', categories: ['Pack de episodios webtoon', 'Ofertas especiales'] },
 ]
